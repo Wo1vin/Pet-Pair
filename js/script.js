85342263f024b8ad2e69f98b7b25cc30fca7d850
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-04-12 15:32:33 -0400

    added functionality, content displays correctly

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
+  document.getElementById('start').innerText = 'Next Pet';
 
   fetch('https://api.rescuegroups.org/v5/public/animals/search/available/haspic/?sort=random&limit=1&fields[attributes]',{
     method: "GET",
@@ -11,16 +12,29 @@ function getFetch(){
   }})
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        let info =data.data[0].attributes;
-        console.log(info);
-        if(!info.ageString){
-        console.log(info.ageGroup)
-        } else { console.log(info.ageString)}
-        console.log(info.name)
-        console.log(info.pictureThumbnailUrl)
+        console.log(data);
+        let info = data.data[0].attributes;
+
+        document.querySelector('h2').innerText = info.name;
+
+        if(info.ageString){
+          document.querySelector('h3').innerText = info.ageString;
+        } else if(!info.ageString){
+          document.querySelector('h3').innerText = info.ageGroup;
+        } else { 
+          document.querySelector('h3').innerText = "Age unknown";
+        } 
+        //find the index that contains attributes.original.url
+        let imgUrl = data.included.find(imgObj).attributes.original.url;
+        document.querySelector('img').src = imgUrl;
+
        // name age location summary picture data[0].attributes.ageString
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+}
+
+function imgObj(obj){
+  return obj.type === 'pictures';
 }
