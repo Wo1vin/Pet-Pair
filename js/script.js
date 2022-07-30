9fed53bf5646a48f5809a058d2db43735be8430b
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-07-29 23:28:21 -0400

    added fluidity and responsiveness. need to finish 900w and below

--- js/script.js
+++ js/script.js
@@ -7,22 +7,42 @@ function start(){
 }
 
 function changeLayout(){
-  const main = document.getElementById('main');
-  main.style.background = "linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,.8) 8%, rgba(255,255,255,.85) 50%, rgba(255,255,255,.8) 92%, rgba(255,255,255,0) 100%)";
-  main.style.flexDirection = "column"
+  const header = document.querySelector('header');
+  header.style.height = "20vh";
 
-  const interact = document.getElementById('interact');
-  interact.style.justifyContent = 'space-evenly';
+  const main = document.getElementById('main').style;
+  main.background = "linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,.8) 8%, rgba(255,255,255,.85) 50%, rgba(255,255,255,.8) 92%, rgba(255,255,255,0) 100%)";
+  main.flexDirection = "column";
+  main.height = "71vh";
+  main.padding = "1em 2em";
+
+  const interact = document.getElementById('interact').style;
+  interact.justifyContent = 'center';
+  interact.height = '8%';
+  interact.width = '70%';
+
+    const start = document.getElementById('start').style;
+    const contact = document.getElementById('contact').style;
+    contact.display = 'inline';
+    const btn = [contact,start];
+
+      btn.forEach(el => {
+        el.fontSize = '2.5rem';
+        el.padding = '.5rem 3rem';
+        el.margin = "0 2%";
+      })
+
+  const petInfo = document.getElementById('petInfo').style;
+  petInfo.display = 'block';
+
+  const imgCont = document.getElementById('imgCont').style;
+  imgCont.visibility = 'visible';
+  imgCont.height = '68%';
+  // imgCont.paddingBottom = '.5rem'
   
-  
-  const contact = document.getElementById('contact');
-  contact.style.display = 'inline-block';
-
-  const petInfo = document.getElementById('petInfo');
-  petInfo.style.display = 'inline';
-
-  const imgCont = document.getElementById('imgCont');
-  imgCont.style.visibility = 'visible';
+  const footer = document.getElementById('footer'); 
+  footer.style.height = '7vh';
+  document.getElementById('footer').querySelector('a').style.fontSize = '2rem';
 
 }
 
