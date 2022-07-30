1ee2e9c8ea6406aaed5bac153a37c02e1daa9f95
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-07-30 16:16:52 -0400

    fixes text and img scalability. need to test for missing picture url

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ function start(){
   getInfo();
   changeLayout();
 }
+const imgCont = document.getElementById('imgCont');
+const petInfo = document.getElementById('petInfo');
 
 function changeLayout(){
   const header = document.querySelector('header');
   header.style.height = "20vh";
-
+  header.querySelector('p').style.fontSize = '3rem';
   const main = document.getElementById('main').style;
   main.background = "linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,.8) 8%, rgba(255,255,255,.85) 50%, rgba(255,255,255,.8) 92%, rgba(255,255,255,0) 100%)";
   main.flexDirection = "column";
@@ -32,18 +34,14 @@ function changeLayout(){
         el.margin = "0 2%";
       })
 
-  const petInfo = document.getElementById('petInfo').style;
-  petInfo.display = 'block';
+  petInfo.style.display = 'block';
 
-  const imgCont = document.getElementById('imgCont').style;
-  imgCont.visibility = 'visible';
-  imgCont.height = '68%';
-  // imgCont.paddingBottom = '.5rem'
+  imgCont.style.visibility = 'visible';
+  imgCont.style.height = '68%';
   
   const footer = document.getElementById('footer'); 
   footer.style.height = '7vh';
-  document.getElementById('footer').querySelector('a').style.fontSize = '2rem';
-
+  document.getElementById('footer').querySelector('a').style.fontSize = '2.2rem';
 }
 
 //retrieve template for APIs and input pet data
@@ -72,9 +70,32 @@ function getInfo(){
         } 
 
         document.querySelector('h4').innerText = "Location: " + data.included.find(findContactInfo).attributes.citystate;
-        //find the index that contains attributes.original.url
-        let imgUrl = data.included.find(findImgUrl).attributes.original.url;
-        document.querySelector('.petPic').src = imgUrl;
+
+        //pulls the image file information from the data. info used: resolutionX, resolutionY, url 
+        let picInfo = data.included.find(findImgInfo).attributes.original;
+        const petPic = imgCont.querySelector('img');
+        if (picInfo.url === undefined){ //if there is no picture
+          imgCont.style.visibility = 'hidden';
+          const noImgAvailable  = document.createElement('h5');
+          noImgAvailable.textContent = `We couldn't get a good picture of ${info.name}, but you can request more information by contacting ${data.included.find(findContactInfo).attributes.name}`;
+
+          petInfo.appendChild(noImgAvailable);
+        } else{
+          // inputs the img url into the img element tag
+          petPic.src = picInfo.url;
+        }
+
+        //establishing variables for image width and height for ease of use
+        let imgWidth = picInfo.resolutionX;
+        let imgHeight = picInfo.resolutionY;
+        // console.log(imgWidth,imgHeight,petPic);
+
+          //if the height of img < 600px, change img object-fit: cover
+          if (imgHeight <= 600){
+            petPic.style.objectFit = 'cover';
+          } else if (imgHeight > 600){
+            petPic.style.objectFit = 'contain';
+          }
 
         document.querySelector('a').href = `mailto:${data.included.find(findContactInfo).attributes.email}?subject=Adoption Inquiry: ${info.name}`;
        // name age location summary picture data[0].attributes.ageString
@@ -89,4 +110,7 @@ function findImgUrl(obj){
 }
 function findContactInfo(obj){
   return obj.type === 'orgs';
+}
+function findImgInfo(obj){
+  return obj.type === 'pictures';
 }
