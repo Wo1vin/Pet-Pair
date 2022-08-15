1eef06483f7b23209e8e7fed2058a3c44f1ef009
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-08-15 01:56:56 -0400

    finished styling and laying out app

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
-//fetch template for APIs
-document.querySelector('button').addEventListener('click', getFetch)
+//calls start function, which calls displayInfo and getInfo functions
+document.querySelector('button').addEventListener('click', start)
 
-function getFetch(){
+function start(){
+  getInfo();
+  changeLayout();
+}
+
+function changeLayout(){
+  const main = document.getElementById('main');
+  main.style.background = "linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,.8) 8%, rgba(255,255,255,.85) 50%, rgba(255,255,255,.8) 92%, rgba(255,255,255,0) 100%)";
+  main.style.flexDirection = "column"
+
+  const interact = document.getElementById('interact');
+  interact.style.justifyContent = 'space-evenly';
+  
+  
+  const contact = document.getElementById('contact');
+  contact.style.display = 'inline-block';
+
+  const petInfo = document.getElementById('petInfo');
+  petInfo.style.display = 'inline';
+
+  const imgCont = document.getElementById('imgCont');
+  imgCont.style.visibility = 'visible';
+
+}
+
+//retrieve template for APIs and input pet data
+function getInfo(){
   document.getElementById('start').innerText = 'Next Pet';
 
   fetch('https://api.rescuegroups.org/v5/public/animals/search/available/haspic/?sort=random&limit=1&fields[attributes]',{
@@ -18,17 +44,17 @@ function getFetch(){
         document.querySelector('h2').innerText = info.name;
 
         if(info.ageString){
-          document.querySelector('h3').innerText = info.ageString;
+          document.querySelector('h3').innerText = "Age: " + info.ageString;
         } else if(!info.ageString){
-          document.querySelector('h3').innerText = info.ageGroup;
+          document.querySelector('h3').innerText = "Age group: "+ info.ageGroup;
         } else { 
           document.querySelector('h3').innerText = "Age unknown";
         } 
 
-        document.querySelector('h4').innerText = data.included.find(findContactInfo).attributes.citystate;
+        document.querySelector('h4').innerText = "Location: " + data.included.find(findContactInfo).attributes.citystate;
         //find the index that contains attributes.original.url
         let imgUrl = data.included.find(findImgUrl).attributes.original.url;
-        document.querySelector('img').src = imgUrl;
+        document.querySelector('.petPic').src = imgUrl;
 
         document.querySelector('a').href = `mailto:${data.included.find(findContactInfo).attributes.email}?subject=Adoption Inquiry: ${info.name}`;
        // name age location summary picture data[0].attributes.ageString
