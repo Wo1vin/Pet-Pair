2f1893da2b2084744ff8bb22bfdb0f95f460388e
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-08-17 14:39:21 -0400

    added mobile fluidity and fixed 'no image' error

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ const imgCont = document.getElementById('imgCont');
 const petInfo = document.getElementById('petInfo');
 
 function changeLayout(){
+  document.getElementById('start').innerText = 'Next Pet';
+
   const header = document.querySelector('header');
   header.style.height = "20vh";
   header.querySelector('p').style.fontSize = '3rem';
@@ -29,11 +31,22 @@ function changeLayout(){
     contact.display = 'inline';
     const btn = [contact,start];
 
-      btn.forEach(el => {
-        el.fontSize = '2.5rem';
-        el.padding = '.5rem 3rem';
-        el.margin = "0 2%";
-      })
+      if (window.innerWidth < 550){
+        btn.forEach(el => {
+          el.fontSize = '2rem';
+          el.padding = '.4rem 2.4rem';
+          el.margin = "0 3%";
+          el.borderRadius = '1.8em';
+          el.fontWeight = '800';
+          el.letterSpacing = ".1em";
+        })
+      } else {
+        btn.forEach(el => {
+          el.fontSize = '2.5rem';
+          el.padding = '.5rem 3rem';
+          el.margin = "0 2%";
+        })
+      }
 
   petInfo.style.display = 'block';
   imgCont.style.height = '68%';
@@ -45,21 +58,19 @@ function changeLayout(){
 
 //retrieve template for APIs and input pet data
 function getInfo(){
-  document.getElementById('start').innerText = 'Next Pet';
-
   fetch('https://api.rescuegroups.org/v5/public/animals/search/available/haspic/?sort=random&limit=1&fields[attributes]',{
     method: "GET",
     headers: {
       "Content-Type":"application/vnd.api+json",
       "Authorization":"Eq0WwliZ"
-  }})
+  },cache: 'reload'})
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data);
+        // console.log(data);
         let info = data.data[0].attributes;
 
         document.querySelector('h2').innerText = info.name;
-
+        // console.log(info.name);
         if(info.ageString){
           document.querySelector('h3').innerText = "Age: " + info.ageString;
         } else if(!info.ageString){
@@ -70,43 +81,42 @@ function getInfo(){
         let orgInfo = data.included.find(findContactInfo).attributes;
         document.querySelector('h4').innerText = "Location: " + orgInfo.citystate;
 
-        console.log(orgInfo.name);
-
-
         const petPic = imgCont.querySelector('img'); //do not add .src to this
         //pulls the image file information from the data. 
         let picInfo = data.included.find(findImgInfo); //dont add attributes
         function findImgInfo(obj){
           return obj.type === 'pictures';
         }
-        
+        let img404 = document.createElement('h5');
         if(!picInfo){
-          // imgCont.style.visibility = 'hidden';
-          console.log("error");
-          petPic.src = 'https://cdn.searchenginejournal.com/wp-content/uploads/2020/08/404-pages-sej-5f3ee7ff4966b.png'
+          petPic.style.display = 'none';
+          console.log("no image found");
+          // petPic.src = 'https://cdn.searchenginejournal.com/wp-content/uploads/2020/08/404-pages-sej-5f3ee7ff4966b.png'
           //or 'https://http.cat/404.png' 
           //or 'https://peoplewithpets.com/wp-content/uploads/2019/11/people-with-pets-no-image-available.jpg'
 
           //create text block
-          let img404 = document.createElement('h5');
           img404.textContent = `We couldn't get a good picture of ${info.name}, but you can request more information by contacting ${orgInfo.name}`;
-          petInfo.appendChild(img404);
+          imgCont.appendChild(img404);
         } else {
-          imgCont.style.visibility = 'visible';
-          console.log('no error');
           petPic.src = picInfo.attributes.original.url;
-          
+          imgCont.style.display = 'inline-flex';
+          imgCont.style.visibility = 'visible';
+          imgCont.querySelector('h5') ? imgCont.querySelector('h5').style.display = 'none': false;
+          petPic.style.display = 'inherit';
+          console.log('no error');
           //establishing variables for image width and height for ease of use
           let imgWidth = picInfo.attributes.original.resolutionX;
           let imgHeight = picInfo.attributes.original.resolutionY;
-            //if the height of img < 600px, change img object-fit: cover
-            if (imgHeight <= 600){
+            //if the height of img < 600px, change img obj(ect-fit: cover
+            if ((imgHeight <= 600) || 
+                (imgHeight < (imgWidth + (imgHeight * 1.2))) ||
+                (imgWidth > imgHeight*1.5)){
               petPic.style.objectFit = 'cover';
             } else {
               petPic.style.objectFit = 'contain';
             }
         }
-
         document.querySelector('a').href = `mailto:${data.included.find(findContactInfo).attributes.email}?subject=Adoption Inquiry: ${info.name}`;
        // name age location summary picture data[0].attributes.ageString
       })
@@ -114,18 +124,6 @@ function getInfo(){
           console.log(`error ${err}`)
       });
 }
-
-function findImgUrl(obj){
-  return obj.type === 'pictures';
-}
 function findContactInfo(obj){
   return obj.type === 'orgs';
 }
-// function findImgInfo(obj){
-//   console.log(obj.type === 'pictures');
-//   for (let i = 0; i <= obj.length ; i++){
-//     if(obj[i].type === 'pictures'){
-//       return;
-//     } else
-// }
-// }
